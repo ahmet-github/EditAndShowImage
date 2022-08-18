e539751fa8e956d640beaa72de9936b7d8b5b205
Ahmet Iyidogan <40362010+ahmet-github@users.noreply.github.com>
2022-08-18 23:31:28 +0300

    Add files via upload

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,22 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Ahmet İyidoğan on 8/12/22.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String){
+        
+        self.name = name
+        self.image = image
+        
+    }
+    
+    
+}
---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Ahmet İyidoğan on 8/12/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+    
+}
